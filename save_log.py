f112db0f0ece58755b01e1efc2229538b3cfd495
motokimano-lab <motokimano@gmail.com>
2026-04-26 16:27:29 +0700

    Update save_log.py

--- save_log.py
+++ save_log.py
@@ -15,30 +15,28 @@ df = pd.read_csv(url)
 usd_jpy = get_fx("JPY=X", 150)
 vnd_jpy = get_fx("VNDJPY=X", 0.006)
 
-# 前処理
-df = prepare_base_dataframe(
-    df,
-    usd_jpy,
-    vnd_jpy
-)
+# -------------------------
+# 先に price を更新する
+# -------------------------
 
-# 価格取得チェック
 warning_tickers = []
 
-for ticker in df["ticker"].unique():
+for idx, row in df.iterrows():
+
+    ticker = row["ticker"]
 
     if ticker == "CASH":
+        df.at[idx, "price"] = 1
         continue
 
-    fallback_price = df.loc[
-        df["ticker"] == ticker,
-        "cost_price"
-    ].iloc[0]
+    fallback_price = row["cost_price"]
 
     price, used_fallback = get_price(
         ticker,
         fallback_price
     )
+
+    df.at[idx, "price"] = price
 
     if used_fallback:
         warning_tickers.append(ticker)
@@ -46,9 +44,19 @@ for ticker in df["ticker"].unique():
 if warning_tickers:
     print(
         f"価格取得失敗（fallback使用）→ "
-        f"{', '.join(warning_tickers)}"
+        f"{', '.join(set(warning_tickers))}"
     )
 
-# 保存実行
+# -------------------------
+# price更新後に前処理
+# -------------------------
+
+df = prepare_base_dataframe(
+    df,
+    usd_jpy,
+    vnd_jpy
+)
+
+# 保存
 result = save_daily_log_detail(df)
 print(result)
