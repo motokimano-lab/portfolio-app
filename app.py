b723f463d9ec6459a427074c512ec4cb8662e4f1
motokimano-lab <motokimano@gmail.com>
2026-05-01 14:49:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import plotly.express as px
 from datetime import datetime
 from zoneinfo import ZoneInfo
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 import os
 import json
 from functions import ( 
